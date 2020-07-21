ca23ff4a96721647040b1aaf461f38ff7a77cff6
Arnie <arniekidd@gmail.com>
2020-07-21 13:05:32 -0500

    submission for deadline

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,33 @@
 class Review
-  
+
+    attr_reader :customer, :rating
+    
+    @@all_reviews=[]
+
+    def initialize(customer, restaurant, rating)
+        @customer=customer
+        @restaurant=restaurant
+        @rating=rating
+
+        restaurant.restaurant_reviews << rating
+        restaurant.restaurant_customers << customer
+        customer.customer_restaurants << restaurant
+    end
+
+    def review_rating
+        @rating
+    end
+
+    def all
+        @@all_reviews
+    end
+
+    def review_customer
+        @customer
+    end
+
+    def review_restaurant
+        @restaurant
+    end
+
 end
