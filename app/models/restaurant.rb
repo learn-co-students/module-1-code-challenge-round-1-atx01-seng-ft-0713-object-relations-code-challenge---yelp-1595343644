ee76c91b77e553257b78ef1874a36e0ce314d2b6
mdivincenzo88 <mdivincenz@hotmail.com>
2020-07-21 13:08:56 -0500

    submission for deadline

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,29 @@
 class Restaurant
-  attr_reader :name
+  attr_reader :name, :review
 
   def initialize(name)
     @name = name
   end
+
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+end
+end
+
+
+def customers
+    self.reviews.map do |reviews|
+      reviews.customer
+    end.uniq
+end
+
+def average_star_rating
+  review_sum = reviews.sum do |review|
+    review.rating
+  end 
+  review_sum / (reviews.count)
+end
 
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,26 @@
 class Review
-  
+    attr_accessor :restaurant, :rating
+    attr_reader :customer
+    @@all= []
+         
+    def initialize(customer, restaurant, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @rating = rating.to_i
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def customer
+        @customer
+    end
+
+    def restaurant
+        @restaurant
+    end
+
 end
+
