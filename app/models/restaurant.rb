8dcadf381e154097f64528e8851634851671d118
micahmosley <micah.mosley@utexas.edu>
2020-07-21 13:04:29 -0500

    submission for deadline

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,4 +5,23 @@ class Restaurant
     @name = name
   end
 
+  def reviews
+    Review.all.select do |review| 
+      review.restaurant==self
+    end 
+  end 
+
+  def customers 
+    reviews.map do |review| 
+      review.customer
+    end.uniq
+  end 
+
+  def average_star_rating
+    total=reviews.sum do |review| 
+      review.rating 
+    end
+    total.to_f/reviews.length.to_f
+  end 
+
 end
