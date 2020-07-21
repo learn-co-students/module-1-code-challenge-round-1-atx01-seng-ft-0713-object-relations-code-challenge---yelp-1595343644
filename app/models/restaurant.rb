1d84d251464f4acab9b2453905345d74fba51466
jtskidmore <johntoddskidmore@gmail.com>
2020-07-21 13:07:57 -0500

    code challenge 1

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,23 @@
+
+
 class Restaurant
+
   attr_reader :name
 
   def initialize(name)
     @name = name
+  end
+
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    reviews.map {|review| review.customer}.uniq
+  end
+
+  def average_star_rating
+    reviews.map {|review| review.rating.to_f}.sum/reviews.length
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,16 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+scott = Customer.new("Scott", "Adams")
+daveN = Customer.new("Dave", "Navarro")
+daveC = Customer.new("Dave", "Castillo")
+
+palios = Restaurant.new("Palios")
+
+review1 = Review.new(scott, palios, 5)
+review2 = Review.new(daveN, palios, 6)
+review3 = Review.new(daveN, palios, 7)
+review4 = Review.new(daveC, palios, 3)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
